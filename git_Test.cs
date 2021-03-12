c23777425ca78b6cf797de203cc726b3ac345b64
pose2don <megacoin8@gmail.com>
2021-03-12 18:54:59 +0900

    first

---
+++ git_Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class git_Test : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("깃허브에 올려보자"); 
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
