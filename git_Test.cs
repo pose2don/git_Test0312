c18438d2bcf61046c160d60d95f3dff5bbc0c2a5
jiwoo <sgo8308@naver.com>
2021-03-12 19:24:06 +0900

    dd

--- git_Test.cs
+++ git_Test.cs
@@ -7,9 +7,7 @@ public class git_Test : MonoBehaviour
     
     void Start()
     {
-        Debug.Log("깃허브에 올려 손지우 김성덕");
-
-        Debug.Log("나도 올릴거야 모지호 모지호");
+        Debug.Log("깃허브에 올");
     }
 
    
