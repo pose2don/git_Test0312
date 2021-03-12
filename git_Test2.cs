af67785601cbce573cb9c1d53812974d812db28c
pose2don <megacoin8@gmail.com>
2021-03-12 19:44:17 +0900

    second file

---
+++ git_Test2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class git_Test2 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("두번째 만든 테스트 파일");
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
