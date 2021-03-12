9927bb2947e59da10ed8d3b69a55a88b62679897
mojiho <73588056+mojiho@users.noreply.github.com>
2021-03-12 19:25:43 +0900

    ㅁㄴㅇ

--- git_Test.cs
+++ git_Test.cs
@@ -10,6 +10,8 @@ public class git_Test : MonoBehaviour
         Debug.Log("깃허브에 올려 손지우 김성덕");
 
         Debug.Log("나도 올릴거야 모지호 모지호");
+
+        Debug.Log("난 내친구를 믿었던 만큼 널 믿었었기에 그런만남이 있은후로 부터");
     }
 
    
