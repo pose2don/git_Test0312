011ae26206d12d38c807532326922044ba39eab7
mojiho <73588056+mojiho@users.noreply.github.com>
2021-03-12 19:10:41 +0900

    모죠업

--- git_Test.cs
+++ git_Test.cs
@@ -7,7 +7,8 @@ public class git_Test : MonoBehaviour
     
     void Start()
     {
-        Debug.Log("깃허브에 올려 손지우"); 
+        Debug.Log("깃허브에 올려 손지우");
+        Debug.Log("깃허브에 올려 모지호");
     }
 
    
