6fc383ee392f39d277097d2223f6650f65beab00
mojiho <73588056+mojiho@users.noreply.github.com>
2021-03-12 19:20:26 +0900

    모죠 커밋

--- git_Test.cs
+++ git_Test.cs
@@ -8,7 +8,9 @@ public class git_Test : MonoBehaviour
     void Start()
     {
 
-        Debug.Log("깃허브에 올려 손지우 김성덕"); 
+        Debug.Log("깃허브에 올려 손지우 김성덕");
+
+        Debug.Log("나도 올릴거야 모지호 모지호");
 
     }
 
